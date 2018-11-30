f7586e6782c626abd0eb29cc687e8a191470d29e
EMILYHLANE <embem80@gmail.com>
2018-11-29 16:04:05 -0800

    Updates server rendered client with standalone client

---
+++ src/components/Posts.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Post from "./Post";
+import axios from "axios";
+
+const baseURL = "https://ehl-motivatr.herokuapp.com";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get(`${baseURL}/api/post/`).then(res => {
+      const posts = res.data;
+      this.setState({ posts });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Post posts={this.state.posts} />
+      </div>
+    );
+  }
+}
+
+export default Posts;
