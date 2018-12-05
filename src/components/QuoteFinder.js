b2be86305aec49ce5f125e89c49145326537c986
EMILYHLANE <embem80@gmail.com>
2018-12-05 12:11:37 -0800

    adds quotes by jack handey api, minimal style for quotes finder page

--- src/components/QuoteFinder.js
+++ src/components/QuoteFinder.js
@@ -3,24 +3,30 @@ import axios from "axios";
 
 class QuoteFinder extends Component {
   state = {
-    QOD: null,
+    jackHandey: [],
     ronSwanson: []
   };
 
   componentDidMount() {
     axios
-      .get("http://quotes.rest/qod.json?category=inspire")
+      .get(
+        "https://api.mlab.com/api/1/databases/jackhandeyapi/collections/quotes?apiKey=gViXTt2ltpcF0a-Ot-Glb5w577mRXb0p"
+      )
       .then(res => {
-        const QOD = res.data.contents.quotes[0].quote;
-        this.setState({ QOD });
-        console.log(res);
+        const data = res.data;
+        const jackHandey = data[0].jackhandey.slice(0, 20);
+        // console.log(jackHandey);
+        const newArray = jackHandey.map(item => {
+          return item.quote;
+        });
+        this.setState({ jackHandey: newArray });
       })
       .catch(err => {
         console.log(err);
       });
 
     axios
-      .get("https://ron-swanson-quotes.herokuapp.com/v2/quotes/10")
+      .get("https://ron-swanson-quotes.herokuapp.com/v2/quotes/25")
       .then(res => {
         const ronSwanson = res.data;
         this.setState({ ronSwanson });
@@ -32,13 +38,25 @@ class QuoteFinder extends Component {
     const ron = ronQuotes.map(quote => {
       return <p key={quote.slice(0, 20)}>{quote}</p>;
     });
+
+    const jackQuotes = this.state.jackHandey;
+    const jack = jackQuotes.map(quote => {
+      return <p key={quote.slice(0, 20)}>{quote}</p>;
+    });
+
     return (
-      <div>
+      <div className="quote-finder-container">
         <h2>Quote Finder</h2>
-        <h3>Quote of the Day via They Said So</h3>
-        <p>{this.state.QOD}</p>
-        <h3>Classic Ron Swanson</h3>
-        {ron}
+        <div className="quote-columns">
+          <div className="ron-quotes">
+            <h3>Classic Ron Swanson</h3>
+            {ron}
+          </div>
+          <div className="jack-quotes">
+            <h3>Deep Thoughts, by Jack Handey</h3>
+            {jack}
+          </div>
+        </div>
       </div>
     );
   }
