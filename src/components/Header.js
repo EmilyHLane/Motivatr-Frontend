20f78f0de0957d113c385f20974a3a622060e854
EMILYHLANE <embem80@gmail.com>
2018-12-04 17:06:42 -0800

    Adds withRouter to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { getJwt } from "../helpers/jwt";
 
 class Header extends Component {
@@ -56,4 +56,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default withRouter(Header);
